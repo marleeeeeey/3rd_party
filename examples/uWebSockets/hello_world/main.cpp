a897248f4bc6a23bea1d34d18fa443f8f7dd3482
marleeeeeey <marleeeeeey@gmail.com>
2026-01-08 18:36:43 +0300

    CMake: add uWebSockets "hello world" example project with minimal setup.

---
+++ examples/uWebSockets/hello_world/main.cpp
@@ -0,0 +1,23 @@
+// Example copied from: https://github.com/uNetworking/uWebSockets/blob/master/examples/HelloWorld.cpp
+
+#include "App.h"
+
+/* Note that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL support */
+
+int main() {
+  /* Overly simple hello world app */
+  uWS::SSLApp({.key_file_name = "server.key",
+               .cert_file_name = "server.crt",
+               .passphrase = "123Qwe!"})
+      .get("/*", [](auto* res, auto* /*req*/) {
+        res->end("Hello world!");
+      })
+      .listen(3000, [](auto* listen_socket) {
+        if (listen_socket) {
+          std::cout << "Listening on port " << 3000 << std::endl;
+        }
+      })
+      .run();
+
+  std::cout << "Failed to listen on port 3000" << std::endl;
+}
