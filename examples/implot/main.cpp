99e9db51582e4c011ccd67a0ef44edba5580bb5f
marleeeeeey <marleeeeeey@gmail.com>
2026-01-23 22:50:12 +0300

    examples/implot: rewrite to use SDL_MAIN_USE_CALLBACKS.

--- examples/implot/main.cpp
+++ examples/implot/main.cpp
@@ -1,20 +1,9 @@
 // based on the original file:
 // https://github.com/ocornut/imgui/blob/master/examples/example_sdl3_sdlrenderer3/main.cpp
-// with some modifications related to implot
 
-// Dear ImGui: standalone example application for SDL3 + SDL_Renderer
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
-// Important to understand: SDL_Renderer is an _optional_ component of SDL3.
-// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
-
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <stdio.h>
 
 #include <cmath>
@@ -24,36 +13,48 @@
 #include "imgui_impl_sdlrenderer3.h"
 #include "implot.h"
 
-#ifdef __EMSCRIPTEN__
-#include "../libs/emscripten/emscripten_mainloop_stub.h"
-#endif
+static constexpr int data_count = 100;
 
-// Main code
-int main(int, char**) {
+struct AppState {
+  SDL_Window* window = nullptr;
+  SDL_Renderer* renderer = nullptr;
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  // Variables for ImPlot demonstration
+  float x[data_count];
+  float y[data_count];
+};
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+  AppState* state = new AppState;
+  *appstate = state;
+
   // Setup SDL
   // [If using SDL_MAIN_USE_CALLBACKS: all code below until the main loop starts would likely be your SDL_AppInit() function]
   if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
     printf("Error: SDL_Init(): %s\n", SDL_GetError());
-    return 1;
+    return SDL_APP_FAILURE;
   }
 
   // Create window with SDL_Renderer graphics context
   float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
   SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-  SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL3+SDL_Renderer example",
-                                        (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
-  if (window == nullptr) {
+  state->window = SDL_CreateWindow("Dear ImGui SDL3+SDL_Renderer example",
+                                   (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
+  if (state->window == nullptr) {
     printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
-    return 1;
+    return SDL_APP_FAILURE;
   }
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-  SDL_SetRenderVSync(renderer, 1);
-  if (renderer == nullptr) {
+  state->renderer = SDL_CreateRenderer(state->window, nullptr);
+  SDL_SetRenderVSync(state->renderer, 1);
+  if (state->renderer == nullptr) {
     SDL_Log("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
-    return 1;
+    return SDL_APP_FAILURE;
   }
-  SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-  SDL_ShowWindow(window);
+  SDL_SetWindowPosition(state->window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+  SDL_ShowWindow(state->window);
 
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
@@ -74,89 +75,65 @@ int main(int, char**) {
   style.FontScaleDpi = main_scale;  // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
 
   // Setup Platform/Renderer backends
-  ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-  ImGui_ImplSDLRenderer3_Init(renderer);
+  ImGui_ImplSDL3_InitForSDLRenderer(state->window, state->renderer);
+  ImGui_ImplSDLRenderer3_Init(state->renderer);
 
-  // Variables for ImPlot demonstration
-  const int data_count = 100;
-  static float x[data_count], y[data_count];
+
+  // Function for ImPlot demonstration
   for (int i = 0; i < data_count; i++) {
-    x[i] = i * 0.1f;
-    y[i] = sinf(x[i]);
+    state->x[i] = i * 0.1f;
+    state->y[i] = sinf(state->x[i]);
   }
 
   // Our state
-  bool show_demo_window = true;
-  bool show_another_window = false;
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  state->show_demo_window = true;
+  state->show_another_window = false;
+  state->clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-  // Main loop
-  bool done = false;
-#ifdef __EMSCRIPTEN__
-  // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-  // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-  io.IniFilename = nullptr;
-  EMSCRIPTEN_MAINLOOP_BEGIN
-#else
-  while (!done)
-#endif
-  {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-    // [If using SDL_MAIN_USE_CALLBACKS: call ImGui_ImplSDL3_ProcessEvent() from your SDL_AppEvent() function]
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      ImGui_ImplSDL3_ProcessEvent(&event);
-      if (event.type == SDL_EVENT_QUIT)
-        done = true;
-      if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
-        done = true;
-    }
+  return SDL_APP_CONTINUE;
+}
 
-    // [If using SDL_MAIN_USE_CALLBACKS: all code below would likely be your SDL_AppIterate() function]
-    if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
-      SDL_Delay(10);
-      continue;
-    }
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+  ImGui_ImplSDL3_ProcessEvent(event);
 
-    // Start the Dear ImGui frame
-    ImGui_ImplSDLRenderer3_NewFrame();
-    ImGui_ImplSDL3_NewFrame();
-    ImGui::NewFrame();
-
-    // Create a simple ImPlot plot
-    ImGui::Begin("ImPlot Example");
-    if (ImPlot::BeginPlot("Sine Wave")) {
-      ImPlot::PlotLine("y = sin(x)", x, y, data_count);
-      ImPlot::EndPlot();
-    }
-    ImGui::End();
-
-    // Rendering
-    ImGui::Render();
-    SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
-    SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    SDL_RenderClear(renderer);
-    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-    SDL_RenderPresent(renderer);
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
   }
-#ifdef __EMSCRIPTEN__
-  EMSCRIPTEN_MAINLOOP_END;
-#endif
 
-  // Cleanup
-  // [If using SDL_MAIN_USE_CALLBACKS: all code below would likely be your SDL_AppQuit() function]
+  return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+  auto* state = static_cast<AppState*>(appstate);
+
+  // Start the Dear ImGui frame
+  ImGui_ImplSDLRenderer3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+
+  // Create a simple ImPlot plot
+  ImGui::Begin("ImPlot Example");
+  if (ImPlot::BeginPlot("Sine Wave")) {
+    ImPlot::PlotLine("y = sin(x)", state->x, state->y, data_count);
+    ImPlot::EndPlot();
+  }
+  ImGui::End();
+
+  // Rendering
+  ImGuiIO& io = ImGui::GetIO();
+  ImGui::Render();
+  SDL_SetRenderScale(state->renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+  SDL_SetRenderDrawColorFloat(state->renderer, state->clear_color.x, state->clear_color.y, state->clear_color.z, state->clear_color.w);
+  SDL_RenderClear(state->renderer);
+  ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), state->renderer);
+  SDL_RenderPresent(state->renderer);
+
+  return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
   ImGui_ImplSDLRenderer3_Shutdown();
   ImGui_ImplSDL3_Shutdown();
   ImPlot::DestroyContext();
   ImGui::DestroyContext();
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
 }
